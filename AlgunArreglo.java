69fa025da027d5bce946e86e4ff9b9ae1f6c16c3
Christiaanz3 <christianadanarellanopena@gmail.com>
2024-12-11 13:53:26 -0700

    Update AlgunArreglo.java

--- AlgunArreglo.java
+++ AlgunArreglo.java
@@ -5,10 +5,10 @@ public class AlgunArreglo {
     public static void main(String[] args) {
         int[] arreglo = { 4, 8, 5, 1};
         System.out.println(Arrays.toString(arreglo));
-        int myVar_temp;
-        for(int myVar_i = 0 ; i < arreglo.length - 1 ; i++)
+        int temp;
+        for(int i = 0 ; i < arreglo.length - 1 ; i++)
         {
-            for(int myVar_j = i + 1 ; j < arreglo.length ; j++)
+            for(int j = i + 1 ; j < arreglo.length ; j++)
             {
         System.out.printl("Valores i:" + i + " j: " + j);
                 if( arreglo[i] > arreglo[j])
